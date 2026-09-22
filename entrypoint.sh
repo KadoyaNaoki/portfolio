e5d28883a652aea4f3236fe167d9d034e6a089f3
kadoya <kadoya.job@gmail.com>
2026-09-23 07:27:14 +0900

    ログ追加

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo "Running entrypoint.sh..." >> /var/log/entrypoint.log
+chown -R www-data:www-data storage bootstrap/cache
+chmod -R 775 storage bootstrap/cache storage/logs storage/framework
+echo "Permission fixed." >> /var/log/entrypoint.log
+
 # キャッシュクリア
 php artisan view:clear || true
 php artisan config:clear || true
